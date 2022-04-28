1a8b9ba8196229fbbf302c218388f1862dff5fc5
Ed Kubiak <ek33450505@gmail.com>
2022-04-27 22:21:49 -0400

    finished up for the day, 4.27.22

--- models/index.js
+++ models/index.js
@@ -1,43 +1,60 @@
-const Vote = require('./vote');
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-// create associations 
+// create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id'
+});
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-  });
+  foreignKey: 'user_id'
+});
 
-// With these two .belongsToMany() methods in place, we're allowing both the User and Post models to query each other's information in the context of a vote
 User.belongsToMany(Post, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'user_id'
-  });
-  
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
 Post.belongsToMany(User, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'post_id'
-  }); 
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
 
 Vote.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
-  
-User.hasMany(Vote, {
-    foreignKey: 'user_id'
-  });
-  
-Post.hasMany(Vote, {
-    foreignKey: 'post_id'
-  });
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post, Vote };
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
